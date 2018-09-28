e8770e2859f20123f400563024c3376b7cd02b24
Andris Strals <andris.strals@gmail.com>
2018-09-28 09:16:27 +0100

    creating deck by comprehension - bad attempt

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,6 +1,15 @@
 defmodule Cards do
   def create_deck() do
-    ["two", "ace", "king", "jack"]
+    values = ["ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+    "jack", "queen", "king"]
+
+    suits = ["spades", "clubs", "diamonds", "hearts"]
+
+    for suit <- suits do
+      for value <- values do
+        "#{value} of #{suit}"
+      end
+    end
   end
 
   def shuffle(deck) do
